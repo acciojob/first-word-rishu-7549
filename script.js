63637c796822642b4e351d7df83b1ffd05a9b5d6
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-05 00:05:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function firstWord(s) {
   // your code here
+	let str = "";
+	s = s.trim();
+	if(s.length < 1) return '';
+	for(let i=0; i<s.length; i++){
+		if(s[i] !==' '){
+			str += s[i];
+		}
+		else {
+			return str;
+		}
+	}
+	return str;
+
+	// return s.trim().split(' ')[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
+
+
+
